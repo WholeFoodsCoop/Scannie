64c89495a677ec51f057d32db38a77a1f288e5f4
Corey Sather <csather@key.wfco-op.store>
2017-04-03 09:43:49 -0500

    added a couple of temp prods to exceptions

--- dataScanning/scaleCheckPage.php
+++ dataScanning/scaleCheckPage.php
@@ -10,25 +10,25 @@ if (!class_exists('SQLManager')) {
 
 class scaleCheckPage extends ScancoordDispatch
 {
-    
+
     protected $title = "Scale Check";
     protected $description = "[Scale Check] Checks all 3 digit PLUs for scale settings.";
     protected $ui = TRUE;
 
     public function body_content()
-    {           
+    {
         $ret = '';
         include('../config.php');
         include('../common/lib/PriceRounder.php');
         $ret .= '<div class="container">';
         $rounder = new PriceRounder();
         $dbc = new SQLManager($SCANHOST, 'pdo_mysql', $SCANDB, $SCANUSER, $SCANPASS);
-        
+
         if($_GET['upc']) {
             $_GET['upc'] = trim($_GET['upc']);
             $upc = str_pad($_GET['upc'], 13, 0, STR_PAD_LEFT);
         }
-         
+
         /*
         $query = $dbc->prepare("");
         $result = $dbc->execute($query);
@@ -36,7 +36,8 @@ class scaleCheckPage extends ScancoordDispatch
         }
         if ($dbc->error()) echo $dbc->error();
         */
-        
+
+
         $dontCheck = array(
             0,
             0,
@@ -83,8 +84,11 @@ class scaleCheckPage extends ScancoordDispatch
             985,
             917
         );
-         
-        $query = $dbc->prepare("select upc, brand, description, normal_price from products where upc < 1000 and scale = 0 group by upc;");
+
+        $dontCheck[] = '189'; //  Marlenes bulk inventory debacle.
+        $dontCheck[] = '190'; //  Marlenes bulk inventory debacle.
+
+        $query = $dbc->prepare("SELECT upc, brand, description, normal_price FROM products WHERE upc < 1000 AND scale = 0 GROUP BY upc;");
         $result = $dbc->execute($query);
         $data = array();
         $headers = array();
@@ -99,33 +103,33 @@ class scaleCheckPage extends ScancoordDispatch
             }
             $i++;
         }
-        
-        
+
+
         /*  Add a column
         $i = 0;
-        foreach ($data as $k => $array) { 
+        foreach ($data as $k => $array) {
             $newColumnName = 'column_name';
             $data[$i][$newColumnName] = 'data_to_put_into_column';
             $headers[$newColumnName] = $newColumnName;
             $i++;
         }
         */
-        
+
         //Add a flags
         $i = 0;
         $flags = array();
-        foreach ($data as $k => $array) { 
+        foreach ($data as $k => $array) {
             if (in_array($array['upc'],$dontCheck)) {
                 $flags['danger'][] = $i;
             }
             $i++;
         }
-        
+
         $ret .=  '<h4>Bulk Product Scale Settings Check</h4>
             <p>If no table is drawn, there are no bulk products that need to be checked for scale settings.
             Products that appear in table should be investigated.</p>
-            <p><span class="alert-info" style="font-weight: bold;">&nbsp;I.T. </span>&nbsp;if a new item was entered with 
-            a 3 digit PLU and it is not a scale item, you need to add it to the list of exceptions 
+            <p><span class="alert-info" style="font-weight: bold;">&nbsp;I.T. </span>&nbsp;if a new item was entered with
+            a 3 digit PLU and it is not a scale item, you need to add it to the list of exceptions
             <span class="text-info">$dontCheck</span></p>';
         $ret .=  '<div class="panel panel-default" style="width:1000px"><table class="table table-striped">';
         $ret .=  '<thead>';
@@ -136,9 +140,9 @@ class scaleCheckPage extends ScancoordDispatch
         $prevKey = '1';
         $ret .=  '<tr>';
         $i=0;
-        foreach ($data as $k => $array) { 
+        foreach ($data as $k => $array) {
             foreach ($array as $kb => $v) {
-                if ($i!=0) $ret .=  '<td> ' . $v . '</td>';                     
+                if ($i!=0) $ret .=  '<td> ' . $v . '</td>';
             }
             $i++;
             if($prevKey != $k) {
@@ -147,23 +151,23 @@ class scaleCheckPage extends ScancoordDispatch
                 } else {
                     $ret .=  '</tr><tr class="hidden">';
                 }
-            } 
+            }
             $prevKey = $k;
         }
         $ret .=  '</table></div>';
-        
+
         /*
-        $upcLink = "<a href='http://key/git/fannie/item/ItemEditorPage.php?searchupc=" 
+        $upcLink = "<a href='http://key/git/fannie/item/ItemEditorPage.php?searchupc="
                 . $upc . "&ntype=UPC&searchBtn=' target='_blank'>{$upc}</a>";
         */
         if ($dbc->error()) $ret .=  $dbc->error();
         $ret .= '</div>';
-        
+
         return $ret;
     }
-    
+
 }
 
 ScancoordDispatch::conditionalExec();
 
- 
+
