02fa626a8f28441ba0cf33aaaa3b1486a04d7bc5
Corey Sather <csather@key.wfco-op.store>
2017-05-04 13:40:58 -0500

    fixed broken link

--- item/SalesChange/SalesChangeIndex.php
+++ item/SalesChange/SalesChangeIndex.php
@@ -54,7 +54,7 @@ class SalesChangeIndex extends ScancoordDispatch
                     <tr><td class="highlight"><a class="btn" href="SCScanner.php"> Scanner </a></td></tr>                               
                     <tr><td class="highlight"><a class="btn" href="../SignInfoHelper.php"> Sale Item Report <i>info in POS</i></a></td></tr>                                
                     <tr><td class="highlight"><a class="btn" href="http://192.168.1.2/git/fannie/item/CoopDealsLookupPage.php"> Coop Deals Check <i>by item</i> </a></td></tr>                                
-                    <tr><td class="highlight"><a class="btn" href="http://192.168.1.2/scancoord/testing/CoopDealsSearchPage.php"> Coop Deals Check <i>complete list</i> </a></td></tr>                                
+                    <tr><td class="highlight"><a class="btn" href="http://192.168.1.2/scancoord/item/Batches/CoopDealsSearchPage.php"> Coop Deals Check <i>complete list</i> </a></td></tr>                                
                 </table></div></div></div>
         ';
         
