6565f17d65ca71ebc00134dffed08537b74d1440
Corey Sather <csather@key.wfco-op.store>
2017-07-05 14:49:00 -0500

    overhaul: moved js to scannie.js

--- common/ui/MenuClass.php
+++ common/ui/MenuClass.php
@@ -27,7 +27,7 @@ class menu
     {
         include('../../config.php');
         $ret = '';
-
+        
         $ret .= '
             <style>
             span.hd.grey {
@@ -216,100 +216,13 @@ class menu
             </div>
         ';
         
-            
         return $ret;
+    }
+    
+    public function cssContent()
+    {
+        
     }
 
 }
 ?>
-
-<script language="javascript" type="text/javascript">
-function popitup(url) {
-	newwindow=window.open(url,'name','height=300,width=300');
-	if (window.focus) {newwindow.focus()}
-	return false;
-}
-
-$(document).ready( function () {
-        $('#searchbar').keypress( function () {
-            var text = $("#searchbar").val();
-            if (text.length) {
-                //alert(text);
-                getSearchResults(text);
-            } else {
-                $('#search-resp').html('')
-            }
-        });
-        backToTop();
-        getMobileMenu();
-        closeMenu();
-});
-
-function getSearchResults(search)
-{
-    $.ajax({
-        url: '../common/ui/searchbar.php',
-        //dataType: 'POST',
-        data: 'search='+search,
-        success: function(response)
-        {
-            $('#search-resp').html(response);
-        }
-    });
-}
-
-function calcView(name)
-{
-    if ( $('#'+name).is(":visible") ) {
-        $('#'+name).hide();
-    } else {
-        $('#'+name).show();
-    }
-}
-
-function backToTop()
-{
-    $(window).scroll(function () {
-        var scrollTop = $(this).scrollTop();
-        //alert(scrollTop);
-        if (scrollTop != 0) {
-            $('#backToTop').show();
-        } else {
-            $('#backToTop').hide();
-        }
-
-        $('.background1, .background2').each(function() {
-            var topDistance = $(this).offset().top;
-
-            if ( (topDistance+100) < scrollTop ) {
-                alert( $(this).text() + ' was scrolled to the top' );
-            }
-        });
-
-        if ($(window).scrollTop() > $('body').height() / 2) {
-            $('#backToTop').show();
-        }
-    });
-
-    $('#backToTop').click(function(){
-        $("html, body").animate({ scrollTop: 0 }, "fast");
-    });
-}
-
-function getMobileMenu()
-{
-    $('#mobileMenuBtn').click( function () {
-        $('#mobileMenu').show();
-    });
-}
-
-function closeMenu()
-{
-    $('#closeMenu').click( function () {
-        $('#mobileMenu').hide();
-    });
-}
-</script>
-
-
-
