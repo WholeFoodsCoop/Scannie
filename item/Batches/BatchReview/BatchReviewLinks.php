009ee4d1d435db1c8de36cdbfde95892d19d4b9c
Corey Sather <csather@key.wfco-op.store>
2017-12-05 13:39:46 -0600

    added sales review; retain id when switching page

--- item/Batches/BatchReview/BatchReviewLinks.php
+++ item/Batches/BatchReview/BatchReviewLinks.php
@@ -1,10 +1,21 @@
 <?php
+$id = $_GET['id'];
+if ($id) {
+    $curID = "?id=$id";
+} else {
+    $curID = '';
+}
 $curRet = '
 <div class="container">
   <ul class="nav nav-tabs" style="boder:1px dotted black">
-    <li class="nav-tabs '.get_active_tab($curPage,'BatchReviewPage.php').'"><a href="BatchReviewPage.php">Non-UNFI Review</a></li>
-    <li class="nav-tabs '.get_active_tab($curPage,'BatchReviewPageUNFI.php').'"><a href="BatchReviewPageUNFI.php">UNFI Reivew</a></li>
-    <li class="nav-tabs '.get_active_tab($curPage,'BatchReviewPageMilk.php').'"><a href="BatchReviewPageMilk.php">UNFI-MILK Review</a></li>
+    <li class="nav-tabs '.get_active_tab($curPage,'BatchReviewPage.php').'">
+        <a href="BatchReviewPage.php'.$curID.'">Non-UNFI Review</a></li>
+    <li class="nav-tabs '.get_active_tab($curPage,'BatchReviewPageUNFI.php').'">
+        <a href="BatchReviewPageUNFI.php'.$curID.'">UNFI Reivew</a></li>
+    <li class="nav-tabs '.get_active_tab($curPage,'BatchReviewPageMilk.php').'">
+        <a href="BatchReviewPageMilk.php'.$curID.'">UNFI-MILK Review</a></li>
+    <li class="nav-tabs '.get_active_tab($curPage,'BatchSaleReviewPage.php').'">
+        <a href="BatchSaleReviewPage.php'.$curID.'">Sales Batch Review</a></li>
   </ul>
 </div>
 ';
