394dc0ecc046debf2fb660bec55d906589ff606d
Corey Sather <csather@key.wholefoods.coop>
2018-06-06 10:20:30 -0500

    general fixes

--- item/coopBasicsScanPage.php
+++ item/coopBasicsScanPage.php
@@ -1,4 +1,5 @@
-<!--/*******************************************************************************
+<?php
+/*******************************************************************************
 
     Copyright 2016 Whole Foods Community Co-op.
 
@@ -18,107 +19,85 @@
     in the file LICENSE along with Scannie; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-*********************************************************************************/-->
-<?php
-//session_start();
-?>
-<html>
-<head>
-  <title> Coop Basics Sign Scan </title>
-  <link rel="stylesheet" href="../common/bootstrap/bootstrap.min.css">
-  <link rel="stylesheet" href="../common/css/darkpages.css">
-  <script src="../common/bootstrap/jquery.min.js"></script>
-  <script src="../common/bootstrap/bootstrap.min.js"></script>
-<style>
-</style>
-</head>
-<body>
-
-<?php
-include('../../../../../var/www/html/git/fannie/config.php');
-if (!class_exists('FannieAPI')) {
-    include('../../../../../var/www/html/git/fannie/classlib2.0/FannieAPI.php');
+*********************************************************************************/
+include(__DIR__.'/../config.php');
+if (!class_exists('ScancoordDispatch')) {
+    include(__DIR__.'/../common/ui/CorePage.php');
 }
-/**
-*   @class coopBasicsScanPage  - All Stores.
-*
-*   coopBasicsScanPage checks for Coop Basics
-*   missing signs for Hillside, using the woodshed.AuditScanner data.
-*   Requires list of Coop Basics UPCs to be uploaded to
-*   woodshed'...SaleChangeQueues which currently must be done
-*   manually.
-*
-*   Instructions on use:
-*
-*   1. Upload Coop Basics Checklist to Generic Uploads (Excel Upload in Office).
-*   2. Verify that $saleItems is generating a list of items on sale.
-*   3. Clear the Audit Scanner Queue.
-*	4. Pull up the audit scanner on a handheld device and scan each coop-basics
-*	   item. No buttons pushing is necessary, just scan those items, the scanner will
-*	   save a list of everything that is scanned. Use the 'admin' account.
-*/
-class coopBasicsScanPage
+if (!class_exists('SQLManager')) {
+    include_once(__DIR__.'/../common/sqlconnect/SQLManager.php');
+}
+class coopBasicsScanPage extends ScancoordDispatch
 {
 
-    protected $title = 'Coop Basics Scan Page';
-
-    public function view()
+    public function body_content()
     {
+        include(__DIR__.'/../config.php');
+        $username = scanLib::getUser();
+        $storeID = scanLib::getStoreID();
+        $storename = scanLib::getStoreName($storeID);
+        $dbc = scanLib::getConObj($SCANALTDB);
 
-        include('../config.php');
-        include('../common/lib/scanLib.php');
-        $dbc = new SQLManager($SCANHOST, 'pdo_mysql', $SCANDB, $SCANUSER, $SCANPASS);
-
-        $store_id = scanLib::getStoreID();
-
-        $storeName = scanLib::getStoreName($store_id);
-        echo '<h4>Coop Basics Review for <strong>'.$storeName.'</strong></h4>';
-
-        echo "don't forget to upload Coop Basics checklist to <strong>Generic Upload</strong>.<br>";
+        $ret = "";
+        $heading = "";
+        
+        $userhead =  ($username == false) ? '<div class="alert alert-danger">You must be logged in to use Audit Scan queue to check Coop Basics Signs.</div>'
+            : "<div class='well'>You are logged in as $username</div>";
+        $heading .=  '<h4>Coop Basics Review for <strong>'.$storename.'</strong></h4>';
+        $heading .=  "Don't forget to upload a Coop Basics checklist to <strong>Generic Upload</strong>.<br>";
 
         if (isset($_GET['session'])) {
             $session = substr($_GET['session'],0,-1);
-            echo $session  . "<br>";
+            $ret .=  $session  . "<br>";
         }
 
+        // get a list of BASICS items
         $list = array();
         $products = array();
-        $queryA = ("
-            SELECT g.upc,p.brand,p.description FROM is4c_op.GenericUpload AS g
-            LEFT JOIN products AS p ON g.upc=p.upc
-            WHERE p.store_id = 1;
+        $argsA = array($storeID);
+        $prepA = $dbc->prepare("
+            SELECT g.upc,p.brand,p.description FROM is4c_op.GenericUpload AS g 
+            LEFT JOIN is4c_op.products AS p ON g.upc=p.upc 
+            WHERE p.store_id = ? 
         ");
-        $resA = $dbc->query($queryA);
+        $resA = $dbc->execute($prepA,$argsA);
         while ($row = $dbc->fetchRow($resA))  {
             $list[] = $row['upc'];
             $products[$row['upc']]['brand'] = $row['brand'];
             $products[$row['upc']]['description'] = $row['description'];
         }
+        if ($er = $dbc->error()) $ret .=  "<div class='alert alert-warning>$er</div>";
 
         if (count($list) < 1) {
-            echo '<span class="danger">No scanned items were found.<br>
-                Check that barcodes are in the correct queue.<br>
-                Check your query.</span>';
+            $ret .=  '<div class="alert alert-danger">No scanned items were found.</div>
+                <div class="alert alert-danger">Check that barcodes are in the correct queue.</div>
+                <div class="alert alert-danger">Check your query.</div>';
         }
 
-        $storeID = scanLib::getStoreID();
-		$scanned = array();
-        $queryB = ('SELECT upc FROM woodshed_no_replicate.AuditScanner WHERE username = "admin" and store_id = '.$storeID);
-        $resB = $dbc->query($queryB);
+        // get list of products scanned
+        $scanned = array();
+        $argsB = array($username,$storeID);
+        $prepB = $dbc->prepare("
+            SELECT upc FROM woodshed_no_replicate.AuditScanner WHERE username = ? and store_id = ?
+        ");
+        $resB = $dbc->execute($prepB,$argsB);
         while ($row = $dbc->fetchRow($resB))  {
             $scanned[] = $row['upc'];
         }
+        if ($er = $dbc->error()) $ret .=  "<div class='alert alert-warning>$er</div>";
 
+        // get list of sale items
         $saleitems = array();
-        $queryC = ('select bl.upc from batchList as bl left join batches as b on bl.batchID=b.batchID where CURDATE() between b.startDate and b.endDate group by upc;');
-        $resC = $dbc->query($queryC);
+        $prepC = $dbc->prepare('select bl.upc from is4c_op.batchList as bl left join is4c_op.batches as b on bl.batchID=b.batchID where CURDATE() between b.startDate and b.endDate group by upc;');
+        $resC = $dbc->query($prepC);
         while ($row = $dbc->fetchRow($resC))  {
             $saleitems[] = $row['upc'];
         }
+        if ($er = $dbc->error()) $ret .=  "<div class='alert alert-warning>$er</div>";
 
-        $missing = array();
-        $remove = array();
-        //  Find tags that are on the floor that should be taken down.
+        $missing = array();  //<--sings that are missing, to put up
+        $remove = array();   //<--signs to take down
+        //  Find tags that are on the floor that should be taken down
         foreach ($scanned AS $key => $upc) {
             if (in_array($upc,$list)) {
                 //  do nothing
@@ -126,7 +105,7 @@ class coopBasicsScanPage
                 $remove[] = $upc;
             }
         }
-        //  Find tags that are missing/need to be put up on the floor.
+        //  Find tags that are missing/need to be put up on the floor
         foreach ($list AS $key => $upc) {
             if (in_array($upc,$scanned)) {
                 //  do nothing - these signs were scanned, they don't need to go up.
@@ -141,11 +120,13 @@ class coopBasicsScanPage
                 //  do nothing
             }
         }
+
         $notInUse = array();
         $curM = date('m');
         foreach ($missing as $key => $upc) {
-            $query = ("select inUse from products where upc = {$upc} and store_id = 1");
-            $res = $dbc->query($query);
+            $args = array($storeID);
+            $query = ("select inUse from products where upc = {$upc} and store_id = ?");
+            $res = $dbc->query($query, $args);
             while ($row = $dbc->fetchRow($res))  {
                 if ($row['inUse'] == 0) {
                     unset($missing[$key]);
@@ -155,36 +136,56 @@ class coopBasicsScanPage
                 }
             }
         }
+        if ($er = $dbc->error()) $ret .=  "<div class='alert alert-warning>$er</div>";
 
         foreach ($missing as $key => $upc) {
             if (is_null($upc)) unset($missing[$key]);
         }
 
-        echo '<br>Signs that are missing<br>----------------------------------<br>';
-        echo '<table class="table">';
+        $tableA .=  '<table class="table table-condensed small"><thead>';
+        $tableA .=  '<tr><th colspan="3" style="text-align: center"><strong>Signs missing from sales floor.</strong></th></tr></thead><tbody>';
         $missingCopyPaste = '';
         foreach ($missing as $upc) {
             $product = "<tr><td>".$upc."</td><td>".$products[$upc]['brand']."</td><td>"
-            .$products[$upc]['description']."</td></tr>";
-            echo $product;
+                .$products[$upc]['description']."</td></tr>";
+            $tableA .=  $product;
             $missingCopyPaste .= $upc . "\n";
         }
-        echo '</table>';
-        echo '<textarea rows="3" cols="15">' . $missingCopyPaste . '</textarea><br />';
-        echo '<table class="table">';
-        echo '<tr><td></td><td></td><td>These signs are on the sales floor and should be taken down</td></tr>';
+        $tableA .=  '</tbody></table>';
+        $tableA .=  '<textarea rows="3" cols="15">' . "MISSING\r\n" . $missingCopyPaste . '</textarea><br />';
+        $tableB .=  '<table class="table table-condensed small"><thead>';
+        $tableB .=  '<tr><th colspan="3">These signs are on the sales floor and should be taken down</th></tr></thead><tbody>';
         $removeCopyPaste = '';
         foreach ($remove as $upc) {
             $product = "<tr><td>".$upc."</td><td>".$products[$upc]['brand']."</td><td>"
-            .$products[$upc]['description']."</td></tr>";
-            echo $product;
+                .$products[$upc]['description']."</td></tr>";
+            $tableB .=  $product;
             $removeCopyPaste .= $upc . "\n";
         }
-        echo "</table>";
-        echo '<textarea rows="3" cols="15">' . $removeCopyPaste . '</textarea><br />';
+        $tableB .=  "</tbody></table>";
+        $tableB .=  '<textarea rows="3" cols="15">' . "TAKE DOWN\r\n" . $removeCopyPaste . '</textarea><br />';
 
-        return false;
+        $html = <<<HTML
+<div class="row">
+    <div class="col-lg-6">
+        <div align="center">$userhead</div>
+    </div>
+    <div class="col-lg-6">
+        $heading
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-6">
+        $tableA
+    </div>
+    <div class="col-lg-6">
+        $tableB
+    </div>
+</div>
+HTML;
+
+        return $html;
     }
 
 }
-coopBasicsScanPage::view();
+ScancoordDispatch::conditionalExec();
